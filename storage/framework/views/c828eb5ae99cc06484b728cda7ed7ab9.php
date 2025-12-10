8a7d06be834c7cd6e9a4ca46c54a8d41b859f6f3
stevanydinda <stevanydinda08@gmail.com>
2025-12-10 10:27:23 +0700

    update project terbaru

--- storage/framework/views/c828eb5ae99cc06484b728cda7ed7ab9.php
+++ storage/framework/views/c828eb5ae99cc06484b728cda7ed7ab9.php
@@ -68,12 +68,12 @@
                         Hotel Aston terletak di pusat kota dengan akses mudah ke tempat wisata dan pusat perbelanjaan.
                     </p>
                 </div>
-
                 <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.855968706255!2d106.800974274991!3d-6.282684793710258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f12f52d509cd%3A0x4292d80b76b2e146!2sASTON%20Bogor%20Hotel%20%26%20Resort!5e0!3m2!1sid!2sid!4v1731641561234!5m2!1sid!2sid"
-                    class="rounded-xl shadow-lg w-full max-w-3xl h-80" style="border:0;" allowfullscreen="" loading="lazy"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.6842170308515!2d106.83309747574223!3d-6.930083967806862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c5ee5f871091%3A0xc58549234bdf7d7c!2sAston%20Bogor%20Hotel%20%26%20Resort!5e0!3m2!1sid!2sid!4v1700000000000!5m2!1sid!2sid"
+                    width="900" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
+
             </section>
 
             <section id="eksplor" class="py-16 bg-gray-50 text-center">
